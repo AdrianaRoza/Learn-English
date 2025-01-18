d1a8d27c1bed4973fa07379a18d04c9d48e8cb00
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-01-18 14:27:06 -0400

    Adding CORS middleware

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from fastapi import FastAPI
-from api.routers import user_router 
+from api.routers import user_router
+from api.routers import english_router
 from api.database import db
 
 
@@ -14,6 +16,15 @@ def init_app()-> FastAPI:
         version="1"
     )
 
+    # Adicionando o middleware de CORS
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Permite todas as origens. Substitua com origens específicas em produção.
+        allow_credentials=True,
+        allow_methods=["*"],  # Permite todos os métodos HTTP
+        allow_headers=["*"],  # Permite todos os cabeçalhos
+    )
+
     @app.on_event("startup")
     async def startup():
         await db.create_all()
@@ -23,7 +34,7 @@ def init_app()-> FastAPI:
         await db.close()
 
     app.include_router(user_router.router, prefix="/users", tags=["Users"])        
-
+    app.include_router(english_router.router, prefix="/english", tags=["English"])
     return app
 
 app = init_app()
